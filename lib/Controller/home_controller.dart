bc833022579f57f2693a0fdd55bde4f598e99e3d
clever-code-007 <smitkevadiya50@gmail.com>
2021-12-07 11:20:13 +0530

    Add responsive framework and GetStorage

--- lib/Controller/home_controller.dart
+++ lib/Controller/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:get_x_demo/Model/home_model.dart';
+import 'package:get_x_demo/main.dart';
 
 class HomeController extends GetxController {
   @override
@@ -11,10 +12,22 @@ class HomeController extends GetxController {
   static HomeController get to => Get.find();
 
   RxInt count = 0.obs;
+  RxString nameFromStorage = "".obs;
 
   Rx<HomeModel> homeModel = HomeModel().obs;
 
   apiCall() {
     homeModel.value = HomeModel.fromJson({"success": 10});
+  }
+
+//For Local Storage
+  setDataInLocal() {
+    //storage is Object from Main.dart File
+    storage.write("name", "smit");
+  }
+
+  getDataInLocal() {
+    //storage is Object from Main.dart File
+    nameFromStorage.value = storage.read("name");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:get_x_demo/View/home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+//For use Stored Data in LocalStorage(Mobile Storage)
+GetStorage storage = GetStorage();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      //builder use for Responsive Framework
+      //Copy Past GetMaterialApp and past your Main.dart file and replase HomePage() to your Page(Screen)
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget!),
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(480, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
       home: HomePage(),
     );
   }
